3d912ff358a9d6a7d681a7ce52e26af3bc08ac49
Sailor-Ham (MacBook Air) <sangja234@gmail.com>
2026-03-26 23:41:16 +0900

    ✅ test(ngrinder): nGrinder 테스트 파일 작성 및 VUser 인원 수 변경 #10
    - 500 / 750 / 1000 → 500 / 800 / 1000

--- docs/scripts/ngrinder/EnrollmentBurstPessimistic800Test.groovy
+++ docs/scripts/ngrinder/EnrollmentBurstPessimistic800Test.groovy
@@ -20,7 +20,7 @@ import java.util.Date
 import java.util.TimeZone
 
 @RunWith(GrinderRunner)
-class EnrollmentBurstSpin750Test {
+class EnrollmentBurstPessimistic800Test {
 
     public static GTest test
     public static HTTPRequest request
@@ -42,7 +42,7 @@ class EnrollmentBurstSpin750Test {
     // 실험 파라미터 설정 (테스트 시간: 10초)
     // TODO: 테스트 인원수에 맞게 이 값을 500, 750, 1000으로 변경하세요.
     // public static final int TOTAL_USERS = 500
-    public static final int TOTAL_USERS = 750
+    public static final int TOTAL_USERS = 800
     // public static final int TOTAL_USERS = 1000
 
     public static final int TEST_DURATION_SECONDS = 10
@@ -50,8 +50,8 @@ class EnrollmentBurstSpin750Test {
     // 테스트 전략 선택
     // TODO: 테스트 전략에 맞게 값을 변경하세요.
     // public static final String STRATEGY = "no-lock"
-    // public static final String STRATEGY = "pessimistic-lock"
-    public static final String STRATEGY = "spin-lock"
+    public static final String STRATEGY = "pessimistic-lock"
+    // public static final String STRATEGY = "spin-lock"
     // public static final String STRATEGY = "pub-sub-lock"
 
     public static String targetUrl = "http://${TARGET_IP}:${TARGET_PORT}/v1/enrollments/${STRATEGY}"
