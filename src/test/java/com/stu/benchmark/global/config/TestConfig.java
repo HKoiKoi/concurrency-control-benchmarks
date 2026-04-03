a2b1f70ef8165cde9c878a13fc33d779e3ad5f7a
Sailor-Ham (Mac mini) <sangja234@gmail.com>
2026-04-03 16:07:40 +0900

    ✅ test(zookeeper): Zookeeper Lock을 적용한 수강 신청 로직 테스트 #35

--- src/test/java/com/stu/benchmark/global/config/TestConfig.java
+++ src/test/java/com/stu/benchmark/global/config/TestConfig.java
@@ -3,13 +3,31 @@ package com.stu.benchmark.global.config;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
+@Testcontainers
 @TestConfiguration(proxyBeanMethods = false)
 public class TestConfig {
+
+	@Container
+	public static GenericContainer<?> zookeeperContainer = new GenericContainer<>(
+		DockerImageName.parse("zookeeper:3.9"))
+		.withExposedPorts(2181)
+		.waitingFor(Wait.forListeningPort());
+
+	@DynamicPropertySource
+	static void zookeeperProperties(DynamicPropertyRegistry registry) {
+		zookeeperContainer.start();
+		registry.add("zookeeper.host", zookeeperContainer::getHost);
+		registry.add("zookeeper.port", () -> zookeeperContainer.getMappedPort(2181));
+	}
 
 	@Bean
 	@ServiceConnection(name = "redis")
